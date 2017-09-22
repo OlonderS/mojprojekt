b31e12d9313fad09b397b6306c765da1b3bd697e
OlonderS <kamil.kucma00@gmail.com>
2017-09-22 11:42:07 +0200

    lekcja 22.09

---
+++ cpp/kwadrat.cpp
@@ -0,0 +1,23 @@
+/*
+ * hello.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+
+    int a=0;
+    
+
+    cout <<"Podaj bok a: "<<endl;
+    cin >> a;
+    cout<<"Pole kwadratu = "<<a*a<<endl;
+    cout<<"Obwód kwadratu = "<<4*a<<endl;
+    
+
+	return 0; 
+}
