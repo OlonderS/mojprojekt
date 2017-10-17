ebc7f80da164c9807523351965d74865710d6133
OlonderS <kamil.kucma00@gmail.com>
2017-10-17 20:37:07 +0200

    dokończenie zadania

--- cpp/petle_dwucyfrowe.cpp
+++ cpp/petle_dwucyfrowe.cpp
@@ -15,12 +15,20 @@ int main(int argc, char **argv)
 	int m=0;
     int n=0;
     
-    while(m<10 || m>99 || m<n)
+    while(m<10 || m>99 || n<m)
     {
-        cout<<"Podaj pierwszą liczbę (10-100)";
+        cout<<"Podaj pierwszą liczbę (10-100): ";
         cin>>m;
-        cout<<"Podaj drugą liczbę (10-100)";
+        cout<<"Podaj drugą liczbę (10-100): ";
         cin>>n;
+    }
+    while(m<=n)
+    {
+    if (m%2==0 && m%3==0)
+        {
+            cout<<m<<endl;
+        }
+        m+=2;
     }
     
     return 0;
