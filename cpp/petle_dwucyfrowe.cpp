4125fbdffa13c54ca0caac0fcf641eb11f9fbd14
OlonderS <kamil.kucma00@gmail.com>
2017-10-17 12:05:15 +0200

    lekcja 17.10

---
+++ cpp/petle_dwucyfrowe.cpp
@@ -0,0 +1,28 @@
+/*
+ * petle_dwucyfrowe.cpp
+ * Napisz program "petle_cw4.cpp", który wypisuje wszystkie dwucyfrowe 
+ * liczby parzyste podzielne przez 3 w zakresie<m,n> podanym
+ * przez użytkownika
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	int m=0;
+    int n=0;
+    
+    while(m<10 || m>99 || m<n)
+    {
+        cout<<"Podaj pierwszą liczbę (10-100)";
+        cin>>m;
+        cout<<"Podaj drugą liczbę (10-100)";
+        cin>>n;
+    }
+    
+    return 0;
+}
+
--- cpp/petle_znak.cpp
+++ cpp/petle_znak.cpp
@@ -10,14 +10,33 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-	char zn=0;
+	char zn='t';
     
-    while(zn !='t' && zn !='T' && zn !='n' && zn !='N' )
-    {
-        cout<<"Podaj literę: ";
-        cin>>zn;
-    }
-        cout<<"Brawo zgadłeś!"<<endl;
+    while(zn=='t' || zn=='T' || zn=='n' || zn=='N')
+        {
+            cout<<"Podaj literę: ";
+            cin>>zn;
+        }
+          
+       // if (zn=='t' || zn=='T' || zn=='n' || zn=='N')
+       // ; //instrukcja pusta
+        //else 
+          //  break;
+        
+       
+        
+            //switch(zn)
+            //{
+            //case 't':
+            //case 'T':
+            //case 'n':
+            //case 'N':
+             //   cout <<zn;
+            //default:
+              //  break; //nie zadziała 
+            //}
+        
+        
 
 	return 0;
 }
