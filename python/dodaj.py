067514c4a9bbaa580eee1b32b8bda7e8d2697247
OlonderS <kamil.kucma00@gmail.com>
2017-10-31 13:31:29 +0100

    wprowadzenie python 31.10

---
+++ python/dodaj.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def sumuj(x, y):
+    return x+y
+def odejmij(x, y):
+    return x-y
+def iloraz(x, y):
+    return x/y
+def iloczyn(x, y):
+    return x*y
+a=int(input('Podaj pierwszą liczbę: '))
+b=int(input('Podaj drugą liczbę: '))
+
+print('Suma:', sumuj(a,b))
+print('Różnica:',odejmij(a,b))
+print('Iloraz:', iloraz(a,b))
+print('Iloczyn:',iloczyn(a,b))
