ba8722689fa16b1ea43feafb9f6131e79ddd19f2
OlonderS <kamil.kucma00@gmail.com>
2017-11-24 11:41:22 +0100

    minmax 24/11

--- python/minmax.py
+++ python/minmax.py
@@ -19,6 +19,23 @@ def maksimum(lista):
     return maks
 
 
+def minmax(lista):
+    lmax = []
+    lmin = []
+    indeks = 0
+
+    for indeks in range(int(len(lista) / 2)):
+        if lista[indeks] >= lista[indeks + 1]:
+            lmax.append(lista[indeks])
+            lmin.append(lista[indeks + 1])
+        else:
+            lmax.append(lista[indeks + 1])
+            lmin.append(lista[indeks])
+        indeks += 2
+
+    return lista
+
+
 def losuj(ile, zakres):
     lista = []
     for i in range(ile):
@@ -30,11 +47,12 @@ def main(args):
     ile = int(input("Podaj ilość liczb: "))
     zakres = int(input("Podaj zakres: "))
     lista = losuj(ile, zakres)
+    assert minimum([7, 4, 9, 1, 3, 0]) == 0
+    assert maksimum([7, 4, 9, 1, 3, 0]) == 9
     print(lista)
     print("Minimum: ", minimum(lista))
     print("Maksimum: ", maksimum(lista))
     return 0
-
 
 
 if __name__ == '__main__':
