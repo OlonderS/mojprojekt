cfa6bdabbc6b9b129d864a222bad3795157f2c34
OlonderS <kamil.kucma00@gmail.com>
2017-11-30 19:30:59 +0100

    fib cpp

--- cpp/fibonacci.cpp
+++ cpp/fibonacci.cpp
@@ -11,17 +11,64 @@ int fib_iter(int n)
 {
     int a = 0;
     int b = 1; 
+    int tmp = 0;
     if (n == 0) 
     {
-        cout << 0;
+        cout << a;
     }
-    cout << a;
-    for (int i=2; i<n; i++)
+    else if (n == 1)
+    {
+        cout << b;
+    }
+    else if (n > 1)
+    {
+
+        //cout<<"Wyraz nr. 0 wynosi: 0"<<endl; 
+        //cout<<"Zlota liczba: 0"<<endl;
+        for (int i=1; i<=n; i++)
+        {
+            tmp = b;
+            b = a + b;
+            a = tmp;
+            cout<<"Wyraz nr. "<<i<<" wynosi: "<<a<<endl;
+            cout<<"Zlota liczba: "<<float(b)/float(a)<<endl;
+        }
+    }
+    return 0;
+}
+
+int fib_iter2(int n)
+{
+    int a = 0;
+    int b = 1;
+    int i = 1;
+    int tmp = 0;
+    
+    if (n == 0)
+    {
+        cout<<a;
+    }
+    while(n>0)
+    {
+        tmp = b;
+        b = a + b;
+        a = tmp;
+        
+        cout<<"Wyraz nr. "<<i<<" wynosi: "<<a<<endl;
+        cout<<"Zlota liczba: "<<float(b)/float(a)<<endl;
+        i++;
+        n--;
+    }
+    return 0;
 }
 
 int main(int argc, char **argv)
 {
-	
+	int ile = 0;
+    cout<<"Ilosc liczb w ciagu: ";
+    cin>>ile;
+    fib_iter(ile);
+    //fib_iter2(ile);
 	return 0;
 }
 
