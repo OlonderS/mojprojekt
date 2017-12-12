317cf3d4366418d26918e6e81c698a2fdfc061d0
OlonderS <kamil.kucma00@gmail.com>
2017-12-12 13:30:57 +0100

    rekurencje

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -2,6 +2,14 @@
 # -*- coding: utf-8 -*-
 
 
+def fib_rek(n):
+    # F(1)=1
+    # F(n)=F(n-2) + F(n-1) dla n>1
+    if n < 2:
+        return 1
+    return fib_rek(n - 2) + fib_rek(n - 1)
+
+
 def fib_iter(n):
     """
     Funkcja wyświetla kolejne wyrazy ciągu Fibonacciego
@@ -28,7 +36,7 @@ def fib_iter(n):
     return b
 
 
-#def fib_iter2(n):
+# def fib_iter2(n):
     """
     Funkcja wyświetla kolejne wyrazy ciągu Fibonacciego
     Funkcja zwraca n-ty wyraz ciągu.
@@ -37,36 +45,38 @@ def fib_iter(n):
     F(n) = F(n-2) + F(n-1) dla n > 1
     """
     #a, b = (0, 1)
-    #if n > 1:
-        #print("Wyraz", 1, "wynosi:", a)
-        #print("Wyraz", 2, "wynosi:", b)
-        #for i in range(2, n-1):
-            #a, b = b, a + b
-            #print("Wyraz", i + 1, "wynosi:", b)
-    #elif n == 1:
-        #print("Wyraz", 1, "wynosi:", b)
-    #elif n == 0:
-        #print("Wyraz", 0, "wynosi:", a)
-    #elif n < 0:
-        #print("Podaj całkowitą liczbę dodatnią")
+    # if n > 1:
+    #print("Wyraz", 1, "wynosi:", a)
+    #print("Wyraz", 2, "wynosi:", b)
+    # for i in range(2, n-1):
+    #a, b = b, a + b
+    #print("Wyraz", i + 1, "wynosi:", b)
+    # elif n == 1:
+    #print("Wyraz", 1, "wynosi:", b)
+    # elif n == 0:
+    #print("Wyraz", 0, "wynosi:", a)
+    # elif n < 0:
+    #print("Podaj całkowitą liczbę dodatnią")
+
 
 def fib_iter2(n):
-    a, b = (0 ,1)
+    a, b = (0, 1)
     i = 1
     if n == 0:
         print(a)
     while n > 0:
         a, b = b, a + b
-        print("Wyraz nr.", i,"wynosi:", a,)
+        print("Wyraz nr.", i, "wynosi:", a,)
         print("Złota liczba:", b / a)
         i = i + 1
         n = n - 1
     return b
 
+
 def main(args):
     liczba = int(input("Podaj ilość wyrazów ciągu: "))
-    fib_iter2(liczba)
-
+#   fib_iter2(liczba)
+    print("Zlota liczba: ", fib_rek(liczba))
     return 0
 
 
