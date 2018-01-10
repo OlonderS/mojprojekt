0a77922ee1d5b0c6dfeeb7182630f4961b450e2a
OlonderS <kamil.kucma00@gmail.com>
2018-01-10 12:36:35 +0100

    sort_bin/excel

--- python/sort_wstaw.py
+++ python/sort_wstaw.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from math import floor
 
 def sort_wstaw_min(lista):
     """wersja liniowa"""
@@ -27,41 +28,43 @@ def sort_wstaw_max(lista):
         lista[k + 1] = el
     return lista  # wstawianie elementu
 
-def wyszukaj_bin_it(lista, el):
-    """tylko gdy tabela jest posortowana"""
-    lewy, prawy = 0, len(lista) - 1
+
+def wyszukaj_bin(lewy, prawy, lista, el):
+    """wersja iteracyjna wyszukiwania binarnego,
+    wyszukujemy indeks do wstawienia elementu"""
     while lewy < prawy:
-        srodek = floor((lewy + prawy)) / 2
+        srodek = floor((lewy + prawy) / 2)
         # print(srodek)
         if el <= lista[srodek]:
             prawy = srodek
         else:
             lewy = srodek + 1
-    if lista[lewy] == el:
-        return lewy
-    return -1
+
+    return lewy
+
+
 
 
 def sort_wstaw_bin(lista):
     """wersja binarna"""
     for i in range (1, len(lista)):
         el = lista[i]
-        k = wyszukaj_bin_it(lewy, prawy, lista, el)
+        k = wyszukaj_bin(0, i, lista, el)
 
         # tworzenie listy z wstawionym elementem
-        lista =
+        lista = lista[:k] + [el] + lista[k:i] + lista[i + 1:] # tworzenie listy z części innej listy, wycinanie listy [indeks:kolejność cyfry]
+        print(lista)
+    return lista
 
 
 def main(args):
-    lista = [4, 3, 7, 0, 2, 3, 1, 9]
-    assert sort_wstaw_min([5, 3, 2, 4, 1, 0]) == [0, 1, 2, 3, 4, 5]
-    assert sort_wstaw_max([5, 3, 2, 4, 1, 0]) == [5, 4, 3, 2, 1, 0]
-    print("Lista przed sortowaniem", lista)
-    print("Lista po posortowaniu (rosnąco)", sort_wstaw_min(lista))
-    print("Lista po posortowaniu (malejąco)", sort_wstaw_max(lista))
-    #  1 [3, 4, 7, 0, 2, 3, 1, 9]
-    #  2 [3, 4, 7, 0, 2, 3, 1, 9]
-    #  3 [0, 3, 4, 7, 2, 3, 1, 9] itd
+    lista = [4, 3, 7, 0, 2, 3, 1, 9, -4]
+    # assert sort_wstaw_min([5, 3, 2, 4, 1, 0]) == [0, 1, 2, 3, 4, 5]
+    # assert sort_wstaw_max([5, 3, 2, 4, 1, 0]) == [5, 4, 3, 2, 1, 0]
+    # print("Lista przed sortowaniem", lista)
+    # print("Lista po posortowaniu (rosnąco)", sort_wstaw_min(lista))
+    # print("Lista po posortowaniu (malejąco)", sort_wstaw_max(lista))
+    sort_wstaw_bin(lista)
     return 0
 
 
