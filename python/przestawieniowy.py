4a593f5586fb1a26d32bdc7e873c8c59a4acc54a
OlonderS <kamil.kucma00@gmail.com>
2018-02-08 20:15:45 +0100

    szyfr przes. praca domowa

--- python/przestawieniowy.py
+++ python/przestawieniowy.py
@@ -2,30 +2,38 @@
 # -*- coding: utf-8 -*-
 
 def szyfruj(tekst, klucz):
+    szyfrogram = ""
     reszta = len(tekst) & klucz
     if reszta:
         tekst += (klucz - reszta) * "."
 
-    for i in range(klucz):
-        for j in range(int(len(tekst) / klucz)):
-            print(i + j * klucz, tekst[i + j * klucz])
+    for i in range(klucz):  #  od 0 do 9
+        for j in range(int(len(tekst) / klucz)):  #  od 0 do 3
+            #  print(i + j * klucz, tekst[i + j * klucz])
             szyfrogram += tekst[i + j * klucz]
     return szyfrogram
+    
 
-def deszyfruj
-
-
-
+def deszyfruj(szyfrogram, klucz):
+    tekst = ""
+    for i in range(int(len(szyfrogram) / klucz)):  #  od 0 do 3
+        for j in range(klucz):  #  od 0 do 9
+            tekst +=szyfrogram[i + (j * int(len(szyfrogram) / klucz))]
+            tekst = tekst.replace(".", "")
+            
+    return tekst
 
 
 def main(args):
     tekst = input("Podaj tekst: ")
     klucz = int(input("Podaj klucz: "))
+    
     while (2 * klucz > len(tekst)):
         klucz = int(input("Podaj klucz:"))
+        
     szyfrogram = szyfruj(tekst, klucz)
     print(szyfrogram)
-    #  print(deszyfruj(szyfrogram, klucz))
+    print(deszyfruj(szyfrogram, klucz))
 
     return 0
 
