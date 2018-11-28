da2ee900542c7c70a9cd9c628d4d10b6a63b8469
OlonderS <kamil.kucma00@gmail.com>
2018-11-28 09:42:04 +0100

    orm

--- orm/kwerendy_orm.py
+++ orm/kwerendy_orm.py
@@ -5,22 +5,59 @@
 import os
 from uczniowie_model import *
 
+def kw08():
+    """lista nazwisk uczniów i nazw klas"""
+    query=(Uczen
+        .select(Uczen.nazwisko, Uczen.klasa.klasa)
+        .join(Klasa)
+    )
+    
+    for obj in query:
+        print(obj.nazwisko, obj.klasa.klasa)
+
+
+def kw09():
+    """lista klas i ilośc uczniów"""
+    query=(Uczen
+        .select(fn.Count(Uczen.id).alias('ilu'), Uczen.klasa.klasa)
+        .join(Klasa)
+        .group_by(Klasa)
+        .order_by(SQL('ilu').desc())
+    )
+    
+    for obj in query:
+        print(obj.klasa.klasa, obj.ilu)
+    
+def kw10():
+    """lista przedmiotów i ilośc ocen"""
+    query=(Ocena
+        .select(fn.Count(Ocena.ocena).alias('ile'), Ocena.przedmiot.przedmiot)
+        .join(Przedmiot)
+        .group_by(Przedmiot)
+        .order_by(SQL('ile').desc())
+    )
+    
+    for obj in query:
+        print(obj.przedmiot.przedmiot, obj.ile)
+        
 
 def main(args):
     baza.connect() 
 
     kwerendy = [
         "Uczen.select()",
-        "Uczen.select().where(Uczen.imie=='RafaĹ')",
+        "Uczen.select().where(Uczen.imie=='Rafał')",
         "Uczen.select().where(Uczen.imie.startswith('G'))",
         "Uczen.select().where(Uczen.egz_mat.between(20, 40))",
-        "Uczen.select().where((Uczen.nazwisko=='Piasecki') & (Uczen.imie=='RafaĹ'))",
+        "Uczen.select().where((Uczen.nazwisko=='Piasecki') & (Uczen.imie=='Rafał'))",
         "Uczen.select().order_by(Uczen.egz_mat.asc())",
         "Uczen.select().where(Uczen.plec==1).order_by(Uczen.egz_mat.asc())",
     ]
     
-    for obj in eval(kwerendy[6]):
-        print(obj.nazwisko, obj.imie, obj.egz_mat)
+#   for obj in eval(kwerendy[6]):
+#       print(obj.nazwisko, obj.imie, obj.egz_mat)
+    
+    kw10()
     
     baza.commit()
     baza.close()
